27b629ec011b7ae24ed21877f191bec79acd9224
FominDA <fomindanjj@rambler.ru>
2022-04-29 11:17:16 +0700

    Some work with interface

--- Main.cpp
+++ Main.cpp
@@ -1,41 +1,95 @@
 #include <iostream>
 #include <iomanip>
+#include <conio.h>
 using namespace std;
-
+void Header() {
+    cout << "+----------------------------+\n";
+    cout << '|' << setw(18) << "Main menu" << setw(12) << "|\n";
+    cout << "+----------------------------+\n";
+}
 string GetPassword() {
     string passw;
     cout << "Enter password";
     cin >> passw;
-    //if () {}
-    //else
     return passw;
 }
 void CheckInput() {
 
 }
-bool Mode() {
-    bool mode;
+int Mode() {
+    int mode;
+    system("cls"); // очистка экрана
     cout << "What do you want to do?\nEnter '1' for encryption or '2' for decryption";
     cin >> mode;
     return mode;
 }
 bool CheckLang() {
     int lang;
+    system("cls"); // очистка экрана
     cout << "Choose language\nEnter '1' for English or '2' for Russian";
     cin >> lang;
-    if (lang == 1) { return true; }
-    else if (lang == 2) { return false; }
-    else { CheckInput(); }
+    switch (lang)
+    {
+    case (1): {
+        return true;
+        break;
+    }
+    case (2): {
+        return false;
+        break;
+    }
+    default:
+        cout << "Incorrect input!";
+        break;
+    }
 }
-void Interface(bool lang) {
-    cout << setw(20) << "Hello\n";
-    cout << "The selected language is ";
-    if (lang == 1) { cout << "English"; }
-    else { cout << "Russian"; }
-    //clrscr();
-    //cout << "";
+void ChooseCode() {
+    system("cls"); // очистка экрана
+    cout << "Choose one of the ciphers\n";
+    cout << "1 - " << setw(20) << "4 - " << setw(30) << "7 - \n";
+    cout << "2 - " << setw(20) << "5 - " << setw(30) << "8 - \n";
+    cout << "3 - " << setw(20) << "6 - " << setw(30) << "9 - \n";
+}
+void Interface() {
+    system("cls"); // очистка экрана
+    Header();
+    cout << setw(18) << "Hello\n";
+    cout << "Enter \"1\" for encryption\nEnter \"2\" for decryption\nPress \"Esc\" to exit the programm\n";
+    cout << "+----------------------------+\n";
 }
 int main() {
-    bool lang = 1;
-    Interface(lang);
+    int a;
+START:
+    Interface();
+    cin >> a;
+    switch (a)
+    {
+    case (1): {
+        ChooseCode();
+        break;
+    }
+    case (2): {
+        ChooseCode();
+        break;
+    }
+    default:
+        cout << "Incorrect input!\nPress any key to return to the main menu";
+        _getch();
+        goto START;
+    }
+    //system("cls"); // очистка экрана
+   //cout << "Hello";
+   //int a = Mode();
+   //switch (a) {
+   //case (1): {
+   //    cout << "Encr";
+   //    break;
+   //}
+   //case (2): {
+   //    cout << "Decr";
+   //    break;
+   //}
+   //default: 
+   //    cout << "Incorrect input!";
+   //}
 }
