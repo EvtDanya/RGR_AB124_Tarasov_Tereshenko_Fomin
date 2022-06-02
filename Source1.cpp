d4998632118fcad0930b4d89005c35c27fe8573d
IlyaNSTU <avto007vs@yandex.ru>
2022-06-02 16:39:11 +0700

    start encrypt 5

--- Source1.cpp
+++ Source1.cpp
@@ -1,5 +1,42 @@
+//in ascii en: A = 65 Z = 90 a = 97 z = 122, 0 = 48, 9 = 57
+
 #include <iostream>
- 
+#include <string>
+using namespace std;
+
+string Encrypt4(string cEng, string keyEng) {
+	string Crypted = cEng;  //crypted Crypted
+	int i = 0, counter = 0; //Crypted position counter	
+	string Alphabet = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
+	string cryptoAlphabet = "ZXWVTSRQPNMLKJHGFDCBzxwvtsrqpnmlkjhgfdcb";
+	while (Crypted[i] != '\0') { //value substitution		
+
+		counter++;
+		i++;
+		if (counter == keyEng.length()) { i = 0; }
+	}
+	return Crypted;
+}
+string Decrypt4(string cEng, string keyEng) {
+	string DeCrypted = cEng;  //DeCrypted Crypted
+	int i = 0, ii = 0; //Crypted position counter	
+	while (DeCrypted[i] != '\0') { //value substitution
+
+		ii++;
+		i++;
+		if (ii == keyEng.length()) { ii = 0; }
+	}
+	return DeCrypted;
+}
+
 int main() {
-	std::cout << "hello";
+	string Crypted, cryptedCrypted, decryptedCrypted; //original Crypted
+	cout << "enter Crypted: ";
+	cin >> Crypted;
+	string key;
+	cryptedCrypted = Encrypt4(Crypted, key);
+	cout << cryptedCrypted << endl;
+	decryptedCrypted = Decrypt4(cryptedCrypted, key);
+	cout << decryptedCrypted;
+
 }
