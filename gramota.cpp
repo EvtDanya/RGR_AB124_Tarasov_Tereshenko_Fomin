7a308e2996aef1f998ecc57a7f8aa920da39583c
IlyaNSTU <avto007vs@yandex.ru>
2022-06-02 17:16:33 +0700

    tarabarskaya gramota crypto for consonants only

---
+++ gramota.cpp
@@ -0,0 +1,52 @@
+//in ascii en: A = 65 Z = 90 a = 97 z = 122, 0 = 48, 9 = 57
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Encrypt4(string cEng, string keyEng) {
+	string Crypted = cEng;  //crypted Crypted
+	int i = 0, ii = 0; //Crypted position counter	
+	string Alphabet = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
+	string cryptoAlphabet = "ZXWVTSRQPNMLKJHGFDCBzxwvtsrqpnmlkjhgfdcb";
+	while (Crypted[i] != '\0') { //value substitution		
+		for (char symbol : Alphabet) {
+			if (cEng[i] == symbol) {
+				Crypted[i] = cryptoAlphabet[ii];
+			}
+			ii++;
+		}
+		i++;
+		ii = 0;
+	}
+	return Crypted;
+}
+string Decrypt4(string cEng, string keyEng) {
+	string DeCrypted = cEng;  //DeCrypted Crypted
+	int i = 0, ii = 0; //Crypted position counter	
+	string Alphabet = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
+	string cryptoAlphabet = "ZXWVTSRQPNMLKJHGFDCBzxwvtsrqpnmlkjhgfdcb";
+	while (DeCrypted[i] != '\0') { //value substitution		
+		for (char symbol : Alphabet) {
+			if (cEng[i] == symbol) {
+				DeCrypted[i] = cryptoAlphabet[ii];
+			}
+			ii++;
+		}
+		i++;
+		ii = 0;
+	}
+	return DeCrypted;
+}
+
+int main() {
+	string Crypted, cryptedCrypted, decryptedCrypted; //original Crypted
+	cout << "enter Crypted: ";
+	cin >> Crypted;
+	string key;
+	cryptedCrypted = Encrypt4(Crypted, key);
+	cout << cryptedCrypted << endl;
+	decryptedCrypted = Decrypt4(cryptedCrypted, key);
+	cout << decryptedCrypted;
+
+}
