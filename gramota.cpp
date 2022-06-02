0e7512a7efcfe65a919a6f4156d2e16dcaea9939
IlyaNSTU <avto007vs@yandex.ru>
2022-06-02 17:17:34 +0700

    tarabarskaya gramota for consonants

--- gramota.cpp
+++ gramota.cpp
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 
-string Encrypt4(string cEng, string keyEng) {
+string Encrypt4(string cEng) {
 	string Crypted = cEng;  //crypted Crypted
 	int i = 0, ii = 0; //Crypted position counter	
 	string Alphabet = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
@@ -21,7 +21,7 @@ string Encrypt4(string cEng, string keyEng) {
 	}
 	return Crypted;
 }
-string Decrypt4(string cEng, string keyEng) {
+string Decrypt4(string cEng) {
 	string DeCrypted = cEng;  //DeCrypted Crypted
 	int i = 0, ii = 0; //Crypted position counter	
 	string Alphabet = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
@@ -42,11 +42,10 @@ string Decrypt4(string cEng, string keyEng) {
 int main() {
 	string Crypted, cryptedCrypted, decryptedCrypted; //original Crypted
 	cout << "enter Crypted: ";
-	cin >> Crypted;
-	string key;
-	cryptedCrypted = Encrypt4(Crypted, key);
+	cin >> Crypted;	
+	cryptedCrypted = Encrypt4(Crypted);
 	cout << cryptedCrypted << endl;
-	decryptedCrypted = Decrypt4(cryptedCrypted, key);
+	decryptedCrypted = Decrypt4(cryptedCrypted);
 	cout << decryptedCrypted;
 
 }
