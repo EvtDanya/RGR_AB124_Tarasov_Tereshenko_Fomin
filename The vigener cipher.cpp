2ea4c1d33fd314ee172da8acbdfe375a6819156f
FominDA <fomindanjj@rambler.ru>
2022-06-04 20:54:25 +0700

    При вводе буквы становятся заглавными

--- The vigener cipher.cpp
+++ The vigener cipher.cpp
@@ -12,12 +12,15 @@ string Encrypt7(string m, string k) {
     string vig;
     char buffer;
     for (int ind = 0; ind < m.length(); ind++) {
-        if (m[ind] >= 65 && m[ind] <= 90) {//for small letter
+        if (m[ind] >= 65 && m[ind] <= 90) {//for big letter
             vig += ((m[ind] + k[ind % k.length()]) % 26) + 65;//since we found the remainder of the division by the power of the alphabet
         } //add 65 to get the ciphertext letter itself
-        else if (m[ind] >= 97 && m[ind] <= 122) {//if it's a big letter, make it small
+        else if (m[ind] >= 97 && m[ind] <= 122) {//if it's a small letter, make it big
             buffer = (m[ind] - 97) + 65;
             vig += (((int)buffer + k[ind % k.length()]) % 26) + 65;
+        }
+        else {
+            vig += m[ind];
         }
     }
     return vig;
@@ -25,20 +28,26 @@ string Encrypt7(string m, string k) {
 string Decrypt7(string m, string k) {
     string vig;
     for (int ind = 0; ind < m.length(); ind++) {
-        vig += ((m[ind] - k[ind % k.length()]) + 26) % 26 + 65;
+        if (m[ind] >= 65 && m[ind] <= 90) {
+            vig += ((m[ind] - k[ind % k.length()]) + 26) % 26 + 65;
+        }
+        else {
+            vig += m[ind];
+        }
     }
     return vig;
 }
 int main()
 {
-    string srctext, key, encrypted, decrypted;
-    cout << "Enter string >> ";
-    cin >> srctext;
-    cout << "Enter key >> ";
-    cin >> key;
+    cout << "all letters are converted to uppercase automatically.\n";
+    string text = "BrUTe-force method is a method of random encryption key generation and matching them with the correct one.";
+    string key = "AfffB";
+    string encr, decr;
+    cout << text << endl;
+    //getline(cin, text);
     key = LowToUpForKey(key);
-    encrypted = Encrypt7(srctext, key);
-    cout << "Encrypted text >> " << encrypted;
-    decrypted = Decrypt7(encrypted, key);
-    cout << "\nDecrypted text >> " << decrypted;
+    encr = Encrypt7(text, key);
+    cout << encr << endl;
+    decr = Decrypt7(encr, key);
+    cout << decr << endl;
 }
