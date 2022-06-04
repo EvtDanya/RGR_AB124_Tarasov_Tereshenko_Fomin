0e3c0c7e5dffaa13ac22f92f65f3fe02e18571b8
FominDA <fomindanjj@rambler.ru>
2022-06-04 21:03:16 +0700

    изменил немного код, убрал лишнее

--- The XOR cipher.cpp
+++ The XOR cipher.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include<map>
+#include<vector>
+#include <string>
+#include <sstream>
 using namespace std;
-string LowToUpForKey(string key) {
-    for (auto& i : key) {
-        if (i >= 97 && i <= 122) {
-            i = ((int)i - 97) + 65;
-        }
-    }
-    return key;
-}
 string Encrypt8(string text, string key) {
     string encr;
-    for (int x = 0; x < text.length(); x++)
+    for (int ind = 0; ind < text.length(); ind++)
     {
 
-        encr += text[x] ^ key[x % key.length()];
+        encr += text[ind] ^ key[ind % key.length()];
     }
     return encr;
 }
 string Decrypt8(string text, string key) {
     string decr;
-    for (int x = 0; x < text.length(); x++)
+    for (int ind = 0; ind < text.length(); ind++)
     {
-        decr += text[x] ^ key[x % key.length()];
+        decr += text[ind] ^ key[ind % key.length()];
 
     }
     return decr;
 }
-int main() {
+int main()
+{
     cout << "all letters are converted to uppercase automatically.\n";
-    string text = "Brute-force method is a method of random encryption key generation and matching them with the correct one.";
-    string key = "abcs";
+    string text = "BrUTe-force method is a method of random encryption key generation and matching them with the correct one.*@@)($!@_)+!@";
+    string key = "AfffB";
     string encr, decr;
-    key = LowToUpForKey(key);
+    cout << text << endl;
+    //getline(cin, text);
     encr = Encrypt8(text, key);
     cout << encr << endl;
     decr = Decrypt8(encr, key);
     cout << decr << endl;
+
 }
