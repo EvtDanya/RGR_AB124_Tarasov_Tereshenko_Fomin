77205384927467faab3a668baffa644f0ee5a843
IlyaNSTU <avto007vs@yandex.ru>
2022-06-05 13:46:32 +0700

    Encrypt6 + Decrypt6

---
+++ vernam.cpp
@@ -0,0 +1,25 @@
+//in ascii en: A = 65 Z = 90 a = 97 z = 122, 0 = 48, 9 = 57
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Encrypt6(string cEng) {
+	
+	return Crypted;
+}
+string Decrypt6(string cEng) {
+	
+	return DeCrypted;
+}
+
+int main() {
+	string Crypted, cryptedCrypted, decryptedCrypted; //original Crypted
+	cout << "enter Crypted: ";
+	cin >> Crypted;
+	cryptedCrypted = Encrypt4(Crypted);
+	cout << cryptedCrypted << endl;
+	decryptedCrypted = Decrypt4(cryptedCrypted);
+	cout << decryptedCrypted;
+
+}
